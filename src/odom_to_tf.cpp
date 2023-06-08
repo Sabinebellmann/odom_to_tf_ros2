59e5309c4e46b4899aade36e328aef1dd532a71d
Sabine Bellmann <bellmann@fzi.de>
2023-06-08 15:35:05 +0200

    callback timer

--- src/odom_to_tf.cpp
+++ src/odom_to_tf.cpp
@@ -4,12 +4,28 @@
 #include <nav_msgs/msg/odometry.hpp>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/msg/transform_stamped.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include "rclcpp/rclcpp.hpp"
+#include <tf2_ros/create_timer_ros.h>
 
 using std::placeholders::_1;
 
 class OdomToTF : public rclcpp::Node {
     public:
         OdomToTF() : Node("odom_to_tf") {
+
+              // Timer Callback
+            publishing_rate = 100;
+            if (publishing_rate <= 0)
+            {
+                std::stringstream ss;
+                ss << "Invalid timer_rate of " << std::to_string(publishing_rate) << " using 1.0 instead!";
+                RCLCPP_WARN(this->get_logger(), ss.str().c_str());
+                publishing_rate = 1.0;
+            }
+
+            odom_received=false;
+
             std::string odom_topic;
             frame_id = this->declare_parameter("frame_id", std::string(""));
             child_frame_id = this->declare_parameter("child_frame_id", std::string(""));
@@ -29,25 +45,52 @@ class OdomToTF : public rclcpp::Node {
             }
             sub_ = this->create_subscription<nav_msgs::msg::Odometry>(odom_topic, rclcpp::SensorDataQoS(), std::bind(&OdomToTF::odomCallback, this, _1));
             tfb_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+              //Timer
+            m_timer = this->create_wall_timer(std::chrono::duration<double>(1.0 / publishing_rate),
+                                    std::bind(&OdomToTF::timerCallback, this));
+
+            std::cerr<<"odom to tf initilized"<<std::endl;
         }
     private:
+        bool odom_received;
+        double publishing_rate;
+        rclcpp::TimerBase::SharedPtr m_timer;
         std::string frame_id, child_frame_id;
         std::shared_ptr<tf2_ros::TransformBroadcaster> tfb_;
         rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_;
-        void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) const {
+        nav_msgs::msg::Odometry::SharedPtr m_odom_msg;
 
-            geometry_msgs::msg::TransformStamped tfs_;
-            tfs_.header = msg->header;
-            tfs_.header.stamp = this->now();
-            tfs_.header.frame_id = frame_id != "" ? frame_id : tfs_.header.frame_id;
-            tfs_.child_frame_id = child_frame_id != "" ? child_frame_id : msg->child_frame_id;
-            tfs_.transform.translation.x = msg->pose.pose.position.x;
-            tfs_.transform.translation.y = msg->pose.pose.position.y;
-            tfs_.transform.translation.z = msg->pose.pose.position.z;
+        void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+            m_odom_msg=msg;
+            std::cerr<< "odom received" <<std::endl;
+            odom_received=true;
+        }
+        
+          // Timer Callback
+        void timerCallback()
+        {
+             std::cerr<< "timer callback" <<std::endl;
+             
+            if (odom_received==true){
+                std::cerr<< "time" <<this->get_clock()->now().nanoseconds()<<std::endl;
+                nav_msgs::msg::Odometry::SharedPtr msg=m_odom_msg;
+                geometry_msgs::msg::TransformStamped tfs_;
+                tfs_.header = msg->header;
+                tfs_.header.stamp = this->get_clock()->now();
+                tfs_.header.frame_id = frame_id != "" ? frame_id : tfs_.header.frame_id;
+                tfs_.child_frame_id = child_frame_id != "" ? child_frame_id : msg->child_frame_id;
+                tfs_.transform.translation.x = msg->pose.pose.position.x;
+                tfs_.transform.translation.y = msg->pose.pose.position.y;
+                tfs_.transform.translation.z = msg->pose.pose.position.z;
 
-            tfs_.transform.rotation = msg->pose.pose.orientation;
+                tfs_.transform.rotation = msg->pose.pose.orientation;
 
-            tfb_->sendTransform(tfs_);
+                tfb_->sendTransform(tfs_);
+            }else{
+                std::cerr<<"no odom recived"<<std::endl;
+            }
+    
+
         }
 };
 
